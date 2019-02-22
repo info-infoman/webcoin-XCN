323d8fa43a501f629385a4732e160051982dbb34
info-infoman <info_infoman@bk.ru>
2019-02-22 10:29:37 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var params = require('webcoin-params')
 
 module.exports = params({
-  blockchain: require('./lib/blockchain.js'),
-  net: require('./lib/net.js'),
-  wallet: require('./lib/wallet.js'),
-  versionbits: require('./lib/versionbits.js')
+  blockchain: require('./src/blockchain.js'),
+  net: require('./src/net.js'),
+  wallet: require('./src/wallet.js'),
+  versionbits: require('./src/versionbits.js')
 })
